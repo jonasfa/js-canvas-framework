afa2714bbacb1a514804a6c747706f59d32ace85
Jonas Alves <jonasfa@gmail.com>
2011-08-15 00:34:19 -0300

    adding ViewSpec

---
+++ spec/ViewSpec.js
@@ -0,0 +1,21 @@
+describe("View",
+function() {
+	var view;
+
+	beforeEach(function() {
+		view = new View();
+	});
+	
+	describe("#layout",
+	function() {
+		it("should retain the given values",
+		function() {
+			view.layout(1, 2, 3, 4);
+			expect(view.left).toBe(1);
+			expect(view.top).toBe(2);
+			expect(view.right).toBe(3);
+			expect(view.bottom).toBe(4);
+		});	
+	});
+	
+});
--- src/View.js
+++ src/View.js
@@ -1,2 +1,14 @@
 function View() {
 }
+
+View.prototype.left = 0;
+View.prototype.top = 0;
+View.prototype.right = 0;
+View.prototype.bottom = 0;
+
+View.prototype.layout = function(left, top, right, bottom) {
+	this.left = left;
+	this.top = top;
+	this.right = right;
+	this.bottom = bottom;
+}
